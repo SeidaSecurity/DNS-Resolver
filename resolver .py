3a790529df301294d3219857615b475a7fe0e762
Justin Williams (JW) <tcz11@txstate.edu>
2026-07-02 15:16:39 -0500

    encode_name properly returns length-prefixed labels

--- resolver .py
+++ resolver .py
@@ -5,5 +5,9 @@ import socket
 def build_header():
     return struct.pack("!HHHHHH", random.randint(0, 65535), 0x0100, 1, 0,0,0)
 
-print(build_header().hex(" "))
-print(len(build_header()))
+def encode_name(domain):
+    result = b""
+    for part in domain.split("."):
+        result += struct.pack("B", len(part)) + part.encode()
+    return result + b"\x00"
+
